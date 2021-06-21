b3ce4bf56e045a780dc9700a483815e5fc8a2621
Kane Pham Phu <phamphutaitrainer@gmail.com>
2021-06-21 23:47:58 +0700

    Project First Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# TaskeeperCoreV2
-We appreciate your attention. This server is used to help my own.
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const Index: React.FunctionComponent = () => {
+  return (
+        <h1>Hello Next.js 👋</h1>
+  )
+}
+
+export default I
