6b3fa59384ee2aa68a96c621a6dd4c7c838727ba
Kane Pham Phu <phamphutaitrainer@gmail.com>
2021-06-22 23:04:14 +0700

    Authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) { }
+  constructor(private readonly authService: AuthService) { }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import * as helmet from 'helmet';
 import * as csurf from 'csurf';
 import { ValidationPipe } from '@nestjs/common';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  app.use(csurf());
   await app.listen(3000);
 }
 bootstrap();
