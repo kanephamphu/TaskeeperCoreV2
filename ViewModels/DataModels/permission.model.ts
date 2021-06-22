ad0506b8c4007bce41777ef44b4154464373b325
Kane Pham Phu <phamphutaitrainer@gmail.com>
2021-06-22 18:56:48 +0700

    first settup

--- ViewModels/DataModels/permission.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Permission {
-    read: boolean;
-    create: boolean;
-    update: boolean;
-    delete: boolean;
-}
--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-const Index: React.FunctionComponent = () => {
-  return (
-        <h1>Hello Next.js 👋</h1>
-  )
-}
-
-export default I
