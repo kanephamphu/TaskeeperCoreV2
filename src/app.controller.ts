738d0482c1b2982e72fe4152d580c020951be5b5
Kane Pham Phu <phamphutaitrainer@gmail.com>
2021-06-22 23:46:41 +0700

    Code Format

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,21 @@
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { LocalAuthGuard } from 'src/auth/guards/local-auth.guard';
-import { AuthService } from 'src/auth/auth.service';
+import { Controller, Get, Request, Post, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { LocalAuthGuard } from "src/auth/guards/local-auth.guard";
+import { AuthService } from "src/auth/auth.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly authService: AuthService) { }
+    constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
+    @UseGuards(LocalAuthGuard)
+    @Post("auth/login")
+    async login(@Request() req) {
+        return this.authService.login(req.user);
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
+    @UseGuards(JwtAuthGuard)
+    @Get("profile")
+    getProfile(@Request() req) {
+        return req.user;
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from 'src/app.module';
-import * as helmet from 'helmet';
-import * as csurf from 'csurf';
-import { ValidationPipe } from '@nestjs/common';
-
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "src/app.module";
+import * as helmet from "helmet";
+import * as csurf from "csurf";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(helmet());
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-  await app.listen(process.env.PORT || 3000)
+    const app = await NestFactory.create(AppModule);
+    app.use(helmet());
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableCors();
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
