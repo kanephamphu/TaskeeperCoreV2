63daa3e46fa36c48a74b6c8fffd9b07c53d6b28f
Tai Pham Phu <tai.phamphu@axonactive.com.vn>
2021-06-23 09:51:51 +0700

    master code format

--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -1,25 +1,25 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppController } from "src/app.controller";
+import { AppService } from "src/app.service";
+import { UsersModule } from "src/users/users.module";
+import { AuthModule } from "src/auth/auth.module";
 
-describe('AppService', () => {
-  let appService: AppService;
+describe("AppService", () => {
+    let appService: AppService;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      imports: [AuthModule, UsersModule],
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            imports: [AuthModule, UsersModule],
+            controllers: [AppController],
+            providers: [AppService],
+        }).compile();
 
-    appService = app.get<AppService>(AppService);
-  });
-
-  describe('app service', () => {
-    it('should return "Hello World!"', () => {
-      expect(appService.getHello()).toBe('Hello World!');
+        appService = app.get<AppService>(AppService);
     });
-  });
+
+    describe("app service", () => {
+        it('should return "Hello World!"', () => {
+            expect(appService.getHello()).toBe("Hello World!");
+        });
+    });
 });
