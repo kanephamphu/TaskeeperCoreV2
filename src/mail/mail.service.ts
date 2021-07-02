2c219be4454b09a66e7ad60dba2e25cfacefddee
Tai Pham Phu <tai.phamphu@axonactive.com.vn>
2021-07-02 18:27:10 +0700

    Verify number

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,3 +1,4 @@
+import { User } from "schemas/user/user.schema";
 import { MailerService } from "@nestjs-modules/mailer";
 import { Injectable } from "@nestjs/common";
 
@@ -5,17 +6,18 @@ import { Injectable } from "@nestjs/common";
 export class MailService {
     constructor(private mailerService: MailerService) {}
 
-    async sendUserVerification(token: string) {
-        const url = `example.com/auth/confirm?token=${token}`;
+    async sendUserVerification(user: User) {
+        const url = `https://taskeeperv2.herokuapp.com/auth/confirm?token=${user.verifyInformation.token}`;
 
         await this.mailerService.sendMail({
-            to: "nowayit69@gmail.com",
+            to: `${user.email}`,
             // from: '"Support Team" <support@example.com>', // override default from
             subject: "Welcome to Taskeeper App! Confirm your Email",
-            template: "verification", // `.hbs` extension is appended automatically
+            template: "./verification", // `.hbs` extension is appended automatically
             context: {
-                name: "Tai",
+                name: `${user.firstName}`,
                 url,
+                verifyNumber: `${user.verifyInformation.verifyNumber}`,
             },
         });
     }
