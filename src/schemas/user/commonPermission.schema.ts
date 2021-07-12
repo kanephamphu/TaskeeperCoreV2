bed19c866b2218d4716805cd319016fdb1d3aaf7
Tai Pham Phu <tai.phamphu@axonactive.com.vn>
2021-07-12 12:34:57 +0700

    Common permission

--- src/schemas/user/commonPermission.schema.ts
+++ src/schemas/user/commonPermission.schema.ts
@@ -5,15 +5,15 @@ export class CommonPermission {
     @Prop({ type: Boolean, default: false })
     manage: boolean;
 
-    @Prop({ type: Boolean })
+    @Prop({ type: Boolean, default: true })
     create: boolean;
 
-    @Prop({ type: Boolean })
+    @Prop({ type: Boolean, default: true })
     read: boolean;
 
-    @Prop({ type: Boolean })
+    @Prop({ type: Boolean, default: true })
     update: boolean;
 
-    @Prop({ type: Boolean })
+    @Prop({ type: Boolean, default: false })
     delete: boolean;
 }
