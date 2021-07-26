1b4a14fd27a2339769e11884472c81d80cd8f325
Kane Pham Phu <phamphutaitrainer@gmail.com>
2021-07-26 21:45:04 +0700

    Optional avatar

--- src/dtos/user/createUser.dto.ts
+++ src/dtos/user/createUser.dto.ts
@@ -11,6 +11,7 @@ import {
     MinLength,
     IsObject,
     IsUrl,
+    IsOptional,
 } from "class-validator";
 import { LoginInformation } from "dtos/user/loginInformation.dto";
 
@@ -67,6 +68,7 @@ export class CreateUserDto {
     readonly gender: string;
 
     @IsUrl()
+    @IsOptional()
     readonly avatar?: string;
 
     @IsObject()
